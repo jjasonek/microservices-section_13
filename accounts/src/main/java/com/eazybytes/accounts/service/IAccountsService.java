8f9be2f0d62e7744ec31b8e403907952a163f4fb
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-07-09 22:09:07 +0200

    Initial commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,32 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto - {@link CustomerDto} Object
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Account Details based on a given mobileNumber
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     * @param customerDto - {@link CustomerDto} Object
+     * @return boolean indicating if the update is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
+}
